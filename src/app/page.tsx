1d8cad2a1a92e7842b6ff1ab8c224761e83097a6
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-08-13 20:05:56 -0400

    Portfolio Completed

---
+++ src/app/page.tsx
@@ -0,0 +1,209 @@
+import { HackathonCard } from "@/components/hackathon-card";
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { DATA } from "@/data/resume";
+import Link from "next/link";
+import Markdown from "react-markdown";
+import IconCloud from "@/components/magicui/icon-cloud";
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 flex justify-between">
+            <div className="flex-col flex flex-1 space-y-3">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-2xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+              />
+              <BlurFadeText
+                className="max-w-[400px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert mt-1">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+      <section id="work">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+          </BlurFade>
+          {DATA.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+            >
+              <ResumeCard
+                key={work.company}
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                href={work.href}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? "Present"}`}
+                description={work.description}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold">Education</h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.school}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ResumeCard
+                key={education.school}
+                href={education.href}
+                logoUrl={education.logoUrl}
+                altText={education.school}
+                title={education.school}
+                subtitle={education.degree}
+                period={`${education.start} - ${education.end}`}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+     
+      <section id="projects">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Projects
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my latest work
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications. Here are a few of my
+                  favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="hackathons">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 13}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  Hackathons
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  I like building things
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                I've participated in many hackathons {/* {" "} {DATA.hackathons.length} hackathons,  */}
+                  where innovators from all over the country unite to create amazing projects in just 48-72 hours.
+                  Witnessing the boundless creativity and drive of passionate teams is nothing short of inspiring!
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <BlurFade delay={BLUR_FADE_DELAY * 14}>
+            <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+              {DATA.hackathons.map((project, id) => (
+                <BlurFade
+                  key={project.title + project.dates}
+                  delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+                >
+                  <HackathonCard
+                    title={project.title}
+                    description={project.description}
+                    location={project.location}
+                    dates={project.dates}
+                    image={project.image}
+                    links={project.links}
+                  />
+                </BlurFade>
+              ))}
+            </ul>
+          </BlurFade>
+        </div>
+      </section>
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Connect with Me
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Let’s Start a Conversation
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Whether you have a project idea, a question, or an exciting opportunity, I'd love to hear from you!{" "}
+                <Link
+                  href="mailto:aryanbhardwaj98126@gmail.com"
+                  className="text-blue-500 hover:underline"
+                >
+                   Send me an email 
+                </Link>{" "}
+                and let’s discuss how we can collaborate or explore new opportunities. I’m always open to connecting.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/data/resume.tsx
@@ -0,0 +1,194 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Aryan Bhardwaj",
+  initials: "AB",
+  url: "https://aryanbhardwaj.vercel.app/",
+  location: "Oakville, ON",
+  locationLink: "https://www.google.com/maps/place/oakville",
+  description:
+    "Crafting Solutions, One Line at a Time. Love turning ideas into reality.",
+  summary:
+    "[As a recent Sheridan College grad, and current Headstarter AI SWE Fellow](#education), I specialize in crafting dynamic, user-centric applications and exploring the latest tech trends. My portfolio showcases a [blend of full-stack projects and cutting-edge UI/UX designs](#). Passionate about AI and always eager to learn, I thrive on solving complex problems and pushing the boundaries of technology. [Let’s connect, share ideas, and create something amazing together](#)! 🚀",
+  avatarUrl: "/me.jpg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Express",
+    "Java",
+    "Javascript",
+    "Python",
+    "C",
+    "C#",
+    "AWS",
+    "Firebase",
+    "MongoDB",
+    "Postgres",
+    "Spring Boot",
+    "Github",
+    "Figma"
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+  ],
+  contact: {
+    email: "aryanbhardwaj98126@gmail.com",
+    tel: "+1 289-623-0010",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/AryanBhardwaj789",
+        icon: Icons.github,
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/aryanbhardwaj789/",
+        icon: Icons.linkedin,
+        navbar: true,
+      },
+      Discord: {
+        name: "Discord",
+        url: "https://discordapp.com/users/861160195858497536",
+        icon: Icons.discord,
+        navbar: true,
+      },
+      Resume: {
+        name: "Resume",
+        url: "https://drive.google.com/file/d/1yJMPfHfwHdqYdlm3e0pceFOLUL4Zwu_S/view",
+        icon: Icons.resume,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "mailto:aryanbhardwaj98126@gmail.com",
+        icon: Icons.email,
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Headstarter AI",
+      href: "https://headstarter.co/",
+      badges: [],
+      location: "Remote",
+      title: "Software Engineering Fellow",
+      logoUrl: "/headstarter.jpeg",
+      start: "June 2024",
+      end: "Present",
+      description:
+        "Building 5+ AI apps and APIs using Nextjs, OpenAI, Pinecone, and StripeAPI with 98% accuracy as seen by 1000 users with mentorship from Amazon, Bloomberg, and Capital One engineers on Agile, CI/CD, Git, and Developed projects from design to deployment leading 3 engineering fellows using MVC design patterns.",
+    },
+  ],
+  education: [
+    {
+      school: "Sheridan College, Oakville",
+      href: "https://www.sheridancollege.ca/",
+      degree: "Computer Programming Diploma (PCPRG)",
+      logoUrl: "/sheridan.jpeg",
+      start: "January 2022",
+      end: "April 2024",
+    }
+  ],
+  projects: [
+    {
+      title: "Coffee Express",
+      href: "#",
+      dates: "July 2023 - August 2023",
+      active: true,
+      description:
+        "Coffee Express is a JavaFX app that transforms your coffee experience with real-time price updates and easy customization. Enjoy a seamless ordering journey with an intuitive interface and efficient order management.",
+      technologies: [
+        "JavaFX",
+        "FXML",
+        "Scene Builder",
+        "File Handling",
+        "Polymorphism"
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "httpshttps://github.com/AryanBhardwaj789/CoffeeExpress",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/coffeefx.png",
+      video: "",
+    },
+    {
+      title: "Budget Master",
+      href: "",
+      dates: "June 2023 - July 2023",
+      active: true,
+      description:
+        "Figma UI prototype that simplifies financial management with features like custom budget categories, expense tracking, and budget visualization. Effortlessly set goals and get reminders to stay on top of your finances.",
+      technologies: [
+        "Figma",
+        "Wireframes"
+      ],
+      links: [
+        {
+          type: "Figma",
+          href: "https://www.figma.com/file/Drjb3v8aA2yM2HpRjCWnkg/Budget-Master-UI?type=design&node-id=0%3A1&mode=design&t=F35WUJwcEL4Pd2Z1-1",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/AryanBhardwaj789/BudgetMasterUI",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/budgetui.png",
+      video: "",
+    },
+    {
+      title: "Book Wise",
+      href: "",
+      dates: "November 2023 - December 2023",
+      active: true,
+      description:
+        "Developed an go-to online bookstore built with Java Spring Boot! Dive into a world of books with secure login, easy browsing, and a smooth shopping experience.",
+      technologies: [
+        "Spring Boot",
+        "Thymeleaf",
+        "HTML",
+        "CSS",
+        "Javascript",
+        "MySQL"
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/AryanBhardwaj789/BookWise",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/bookspring.png",
+      video:
+        "",
+    },
+  ],
+  hackathons: [
+    {
+      title: "Hackathon - Headstarter",
+      dates: "July 28, 2024",
+      location: "",
+      description:
+        "Developed a secure password manager application built with React and Next.js, featuring Firebase authentication, password generation, and secure storage.",
+      image:
+        "/headstarter.jpeg",
+      mlh: "",
+      links: [{
+        title: "Github",
+        icon: <Icons.github className="h-4 w-4" />,
+        href: "https://github.com/AryanBhardwaj789/CipherSafe",
+      },
+      ],
+    },
+  ],
+} as const;
